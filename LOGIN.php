201e93b8f11c568783b8e77ee579b427701843be
misung <voielactee7@naver.com>
2023-05-26 22:04:58 +0900

    form 태그

---
+++ LOGIN.php
@@ -0,0 +1,30 @@
+<?php
+
+$id = $_POST["user_id"];
+$password = $_POST["user_password"];
+$password_check = $_POST["user_password_check"];
+$name = $_POST["user_name"];
+$gender = $_POST["user_gender"];
+$email = $_POST["user_email"];
+$email_e = $_POST["user_email_e"];
+// $email_address = $_POST["user_email_address"];
+$address = $_POST["user_address"];
+$detail_address = $_POST["user_detail_address"];
+$cellphone_t = $_POST["cellphone"];
+$cellphone_f = $_POST["cellphone_choice"];
+$cellphone_m = $_POST["cellphone_medium"];
+$cellphone_e = $_POST["cellphone_end"];
+$introduce = $_POST["txt"];
+
+echo "아이디 : ".$id."<br/>";
+echo "비밀번호 : ".$id."<br/>";
+echo "비밀번호 확인 : ".$id."<br/>";
+echo "이름 : ".$name."<br/>";
+echo "성별 : ".$gender."<br/>";
+echo "이메일 : ".$email." @ ".$email_e."<br/>";
+echo "주소 : ".$address."<br/>";
+echo "상세주소 : ".$detail_address."<br/>";
+echo "휴대폰 : ".$cellphone_t." ".$cellphone_f." - ".$cellphone_m." - ".$cellphone_e."<br/>";
+echo " 자기소개 : ".$introduce."<br/>";
+
+?>
---
+++ TEST3.php
@@ -0,0 +1,19 @@
+<?php
+
+$real_id = "test";
+$real_pass = "testpass";
+// 사용자가 입력한 id와 pass를 가져옴
+// $_GET $_POST
+
+$id = $_POST["user_id"]; // $id : 사용자가 입력한 id값
+$pass = $_POST["user_pass"];
+
+if ($id == $real_id && $pass == $real_pass) {
+  echo "id : ".$id."<br/>";
+  echo "pass : ".$pass."<br/>";
+} else {
+  echo "잘못입력";
+}
+
+
+?>
