d584f151d4b1a663c09a34529c850bc552fdf19b
misung <voielactee7@naver.com>
2023-05-28 23:32:09 +0900

    [QUIZ} 100이하의 자연수 n을 입력받고 n개의 정수를 입력받아 평균 출력

--- QUIZ/EX13.php
+++ QUIZ/EX13.php
@@ -3,12 +3,14 @@
 $num = $_POST["num"];
 $sum = 0;
 $avg = 0;
+$cnt = 0;
 
 if ( $num <= 100 ) {
   for ( $i = 1; $i <= $num; $i++ ) {
     $sum += $i;
+    $cnt++;
   }
-  $avg = $sum / $num;
+  $avg = $sum / $cnt;
 }
 
 echo $avg;
